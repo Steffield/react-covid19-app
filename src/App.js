16a3be3147049340a6c114370b75df4c4501c312
Steffield <steffilebby@gmail.com>
2020-05-13 22:34:35 -0400

    component set up, API set up, data rendering

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, {Component} from "react";
+import { Cards, Chart, CountryPicker} from "./components";
+import styles from "./App.modules.css"
+import { fetchData } from "./API";
+
+
+class App extends Component {
+  state = {
+    data: {}
+  }
+
+  async componentDidMount(){
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData })
+  }
+  render() {
+    const { data }= this.state;
+    return (
+      <div className={styles.container}>
+        <Cards data={ data } />
+        <CountryPicker />
+
+        <Chart />
+      </div>
+    )
+  }
+}
+
+export default App;
