f937b8b7d12700e3c63d8aa7c8d1b475c9db27e9
Steffield <steffilebby@gmail.com>
2020-05-14 21:08:55 -0400

    Updates

---
+++ precache-manifest.a3f4c9ea59adc9260005bbea5161eafe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94076722903364dcee01e34975e0ea80",
+    "url": "/react-covid19-app/index.html"
+  },
+  {
+    "revision": "a122123e38232f671742",
+    "url": "/react-covid19-app/static/css/main.5608607b.chunk.css"
+  },
+  {
+    "revision": "8e65599b14c2eb09f005",
+    "url": "/react-covid19-app/static/js/2.ce158ac8.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-covid19-app/static/js/2.ce158ac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a122123e38232f671742",
+    "url": "/react-covid19-app/static/js/main.abce9ac4.chunk.js"
+  },
+  {
+    "revision": "1a0f03cf99349d8558dd",
+    "url": "/react-covid19-app/static/js/runtime-main.bfbc4d15.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react-covid19-app/static/media/covidIMG.d7265326.png"
+  }
+]);
